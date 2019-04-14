d1fdc833c46f06b0512e26195c39b2ba7c221ae7
Leah Miller <lkmiller.87@gmail.com>
2019-04-13 18:56:37 -0600

    Adds test for advice by id

--- spec/services/advice_service_spec.rb
+++ spec/services/advice_service_spec.rb
@@ -19,6 +19,18 @@ describe AdviceService do
     expect(result[:message][:type]).to eq("notice")
     expect(result[:message][:text]).to eq("No advice slips found matching that search term.")
   end
+  it "can return multiple advice slips matching one query" do
+    service = AdviceService.new
+    query = "run"
+
+    result = service.slip_query(query)
+
+    expect(result[:total_results]).to eq("2")
+    expect(result[:slips][0]).to have_key(:advice)
+    expect(result[:slips][0]).to have_key(:slip_id)
+    expect(result[:slips][1]).to have_key(:advice)
+    expect(result[:slips][1]).to have_key(:slip_id)
+  end
   it "can return random advice" do
     service = AdviceService.new
 
@@ -27,5 +39,14 @@ describe AdviceService do
     expect(result.count).to eq(1)
     expect(result[:slip]).to have_key(:advice)
     expect(result[:slip]).to have_key(:slip_id)
+  end
+  it "can return advice by id" do
+    service = AdviceService.new
+    id = 3
+
+    result = service.slip_id(id)
+
+    expect(result[:slip]).to have_key(:advice)
+    expect(result[:slip][:advice]).to eq("Don't eat non-snow-coloured snow.")
   end
 end
