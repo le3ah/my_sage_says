1fa67c8eeb66b87051fe724a50dfbb73ce870ddb
Leah Miller <lkmiller.87@gmail.com>
2019-04-13 20:31:25 -0600

    Adds feature test for searching for advice by string

---
+++ spec/features/user_can_search_advice_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "As a user, visiting the site" do
+  it "sees advice after submitting a query string" do
+    query_string = "world"
+    visit root_path
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_field(:recent_dream)
+
+    fill_in :recent_dream, with: query_string
+
+    click_on("Get Advice")
+    expect(page).to have_content("Smile and the world smiles with you. Frown and you're on your own.")
+  end
+end
