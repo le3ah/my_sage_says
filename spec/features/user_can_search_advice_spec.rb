6ed7cc886e0630d5cb85591a1bd3767f039e7bf6
Leah Miller <lkmiller.87@gmail.com>
2019-04-13 21:00:22 -0600

    Adds sad path test for searching for advice

--- spec/features/user_can_search_advice_spec.rb
+++ spec/features/user_can_search_advice_spec.rb
@@ -13,4 +13,16 @@ describe "As a user, visiting the site" do
     click_on("Get Advice")
     expect(page).to have_content("Smile and the world smiles with you. Frown and you're on your own.")
   end
+  it "sees notice after submitting a query string that does not exist" do
+    query_string = "jobs"
+    visit root_path
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_field(:recent_dream)
+
+    fill_in :recent_dream, with: query_string
+
+    click_on("Get Advice")
+    expect(page).to have_content("No advice slips found matching that search term.")
+  end
 end
